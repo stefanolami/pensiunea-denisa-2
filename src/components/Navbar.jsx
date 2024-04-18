bbfe44bf58bf2cc55f76a8097af1fb500db27e96
stefanolami <stefanolami90@gmail.com>
2024-04-18 13:53:24 +0300

    passing messages to client component wiorking

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-'use client'
+/* 'use client'
 
 import { Link } from '../navigation'
 
@@ -12,6 +12,28 @@ export default function Navbar({ translations }) {
 				<Link href="/deals">{translations[3]}</Link>
 				<Link href="/gallery">{translations[4]}</Link>
 				<Link href="/contacts">{translations[5]}</Link>
+			</nav>
+		</div>
+	)
+} */
+
+'use client'
+
+import { Link } from '../navigation'
+
+export default function Navbar(messages) {
+	console.log(messages)
+	const navTrans = messages?.messages?.Navigation
+
+	return (
+		<div className="flex px-16 py-6 flex-row justify-between items-center">
+			<Link href="/">{navTrans.home}</Link>
+			<nav className="flex flex-row justify-center items-center gap-4">
+				<Link href="/about">{navTrans.about}</Link>
+				<Link href="/prices">{navTrans.prices}</Link>
+				<Link href="/deals">{navTrans.deals}</Link>
+				<Link href="/gallery">{navTrans.gallery}</Link>
+				<Link href="/contacts">{navTrans.contacts}</Link>
 			</nav>
 		</div>
 	)
