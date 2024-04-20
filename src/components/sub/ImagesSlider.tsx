814129f5e605ee0efdbbc466e0eec166ebb7e43a
stefanolami <stefanolami90@gmail.com>
2024-04-20 17:38:37 +0300

    removed dark layer from hero

--- src/components/sub/ImagesSlider.tsx
+++ src/components/sub/ImagesSlider.tsx
@@ -151,7 +151,7 @@ export const ImagesSlider = ({
 			{areImagesLoaded && overlay && (
 				<div
 					className={cn(
-						'absolute inset-0 bg-black/60 z-40',
+						'absolute inset-0  z-40' /* bg-black/60 */,
 						overlayClassName
 					)}
 				/>
