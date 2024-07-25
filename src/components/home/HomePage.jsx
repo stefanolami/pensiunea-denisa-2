cc682d8907893d79438e643703115b02d2984830
stefanolami <stefanolami90@gmail.com>
2024-07-25 18:12:35 +0300

    change from prod to main

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -22,7 +22,7 @@ export default function HomePage() {
 							elit, sed do eiusmod tempor incididunt ut labore et
 							dolore magna aliqua. Ut enim ad minim veniam, quis
 							nostrud exercitation ullamco laboris nisi ut aliquip
-							ex ea commodo consequat.
+							ex ea commodo consequat
 						</p>
 					</div>
 				</section>
